9e81ec7a32feb62acc696610775bc4a56c3fd67d
Ralph Warren Joson <ralph.joson03@gmail.com>
2025-04-09 13:53:30 +0800

    Remove console.log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,10 +93,6 @@ export default function Home() {
     setFiltered(filtered.slice(0, 10));
   };
 
-  useEffect(() => {
-    console.log("state", state);
-  }, [state]);
-
   return (
     <div className="min-h-screen bg-gray-100 p-6 text-center">
       <h1 className="text-3xl font-bold mb-6 text-gray-900">
